c18aa34e745ad3dbe9a3224cf19f4169ec9f1b09
Yonni GUERRAULT <yonni4@gmail.com>
2025-03-30 10:54:47 +0200

    environments

--- src/app/services/km-api.service.ts
+++ src/app/services/km-api.service.ts
@@ -1,18 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.development';
+
+const KM_API_URL = environment.kmApiUrl; // URL de l'API pour le développement
 
 @Injectable({
   providedIn: 'root'
 })
 export class KmApiService {
 
-    private readonly apiUrl = 'http://https://kanji-master-server.onrender.com/test'; // URL de l'API (change pour prod si nécessaire)
-
   constructor(private readonly http: HttpClient) { }
 
   // Méthode pour récupérer le message depuis la route /test
   getTestMessage(): Observable<any> {
-    return this.http.get<any>(this.apiUrl); // Effectuer la requête GET
+    return this.http.get<any>(KM_API_URL); // Effectuer la requête GET
   }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    kmApiUrl: 'https://kanji-master-server.onrender.com/test', // URL de l'API pour le développement
+    // Autres variables d'environnement spécifiques au développement peuvent être ajoutées ici
+};
